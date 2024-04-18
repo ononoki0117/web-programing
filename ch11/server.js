3b8d3b2a82f2e6983af65791fd020abe77ace5da
ononoki0117 <tytttyty@naver.com>
2024-04-18 16:10:12 +0900

    24.04.18.

---
+++ ch11/server.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql2");
+var conn = mysql.createConnection({
+    host:'127.0.0.1',
+    user:'root',
+    password:"dlsrmfh1!",
+    database:"jdbctest",
+});
+
+conn.connect();
+
+const express = require('express');
+const app = express();
+
+conn.query("select * from book", function(err, rows, fields){
+    if (err) throw err;
+    console.log(rows);
+});
+
---
+++ ch12/server.js
@@ -0,0 +1,28 @@
+let mydb;
+
+const mongoclient = require('mongodb').MongoClient;
+const url = 'mongodb+srv://ononoki0117:Kuro11350@test-cluster.kmov3ip.mongodb.net/?retryWrites=true&w=majority&appName=test-cluster';
+
+const express = require('express');
+const app = express();
+
+mongoclient.connect(url).then(client => {
+    console.log('몽고 db 접속 성공');
+
+    mydb = client.db('testboard');
+
+    app.listen(8080, function(){
+        console.log("포트 8080으로 접속 대기중");
+    });
+}).catch(err=>{
+    console.log(err);
+});
+
+app.get('/list', function(req, res){
+    mydb.collection('post').find().toArray().then(result => {
+        console.log(result);
+    })
+})
+
+
+
